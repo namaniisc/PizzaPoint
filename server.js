a2b1cf15eea82fde69f8d3319c91dcfe43779aa9
Naman Saraswat <namaniisc@gmail.com>
2025-03-30 16:46:43 +0530

    Setup Socket Connection
    Added socket.io for real-time communication
    
    Implemented socket connection handling

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')(session)
 const passport = require('passport')
+const Emitter = require('events')
 
 // Database connection
 const url = 'mongodb://localhost/pizza';
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('Database connected...');
@@ -22,12 +22,16 @@ connection.once('open', () => {
     console.log('Connection failed...', err);
 });
 
-
 // Session store 
 let mongoStore = new MongoDbStore({
                 mongooseConnection: connection,
                 collection: 'sessions'
             })
+
+// Event emitter 
+const eventEmitter = new Emitter()
+app.set('eventEmitter', eventEmitter)
+
 // Session config
 app.use(session({
     secret: process.env.COOKIE_SECRET,
@@ -62,6 +66,24 @@ app.set('view engine', 'ejs')
 
 require('./routes/web')(app)
 
-app.listen(PORT , () => {
-    console.log(`Listening on port ${PORT}`)
+const server = app.listen(PORT , () => {
+            console.log(`Listening on port ${PORT}`)
+        })
+
+// Socket 
+
+const io = require('socket.io')(server)
+io.on('connection', (socket) => {
+      // Join  
+      socket.on('join', (orderId) => {
+        socket.join(orderId)
+      })
+})
+
+eventEmitter.on('orderUpdated', (data) => {
+    io.to(`order_${data.id}`).emit('orderUpdated', data)
+})
+
+eventEmitter.on('orderPlaced', (data) => {
+    io.to('adminRoom').emit('orderPlaced', data)
 })
